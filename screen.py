e3c25a7fccf3a337a2d2dd20e937c618c3ef068f
idk <stavivgi0@gmail.com>
2024-09-30 16:34:06 +0300

    eshkar's changes

---
+++ screen.py
@@ -0,0 +1,20 @@
+import pygame, sys
+from pygame import *
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+green= (64,200,45)
+screen = display.set_mode((800, 600))
+
+
+running = True
+
+while running:
+    screen.fill(green)
+    display.flip()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+sys.exit()
+quit()
---
+++ soldier.py
@@ -0,0 +1,26 @@
+
+def soldier_legs (x_index, y_index):
+    x_index1 = x_index + 3
+    x_index2 = x_index1
+    y_index1 = y_index
+    y_index2 = y_index1 + 1
+    tup1 = (x_index1, y_index1)
+    tup2 = (x_index2, y_index2)
+    list_tuple= [tup1,tup2]
+    return list_tuple
+
+print(soldier_legs(0,0))
+
+def soldier_body(x_index, y_index):
+    list = []
+    for i in range (2):
+        list.append((x_index, y_index))
+        counter =0
+        for i in range (3):
+            new_x_index = x_index + 1 + counter
+            counter += 1
+            list.append((new_x_index, y_index))
+        y_index += 1
+    return list
+
+print(soldier_body(0,0))
