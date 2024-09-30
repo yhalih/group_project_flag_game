8282d9dd227144225ffb62f59b953820864de7a2
jbt <stavivgi0@gmail.com>
2024-09-30 15:59:06 +0300

    result initial functions

---
+++ result.py
@@ -0,0 +1,25 @@
+import soldier
+
+
+def end(soldier_location, mine_locations):
+    body_locations = soldier.soldier_body(soldier_location)
+    win = True
+
+    # Finding out if game ended because of win or loss
+    for location in body_locations:
+        if location in mine_locations:
+            win = False
+
+    end_print(win, soldier_location)
+    time.sleep(3)
+    sys.exit()
+    quit()
+
+
+def end_print(win, soldier_location):
+    if win:
+        print("You won!")
+    else:
+        placement('explosion.png', soldier_location)
+        placement('injury', soldier_location)
+        print("You lost!")
